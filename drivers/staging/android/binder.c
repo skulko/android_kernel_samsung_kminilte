b538bf929a9cc14dd9e348a7b0f50203a6be4657
Arve Hjønnevåg <arve@android.com>
2014-02-13 20:25:06 -0800

    Staging: android: binder: More offset validation.
    Make sure offsets don't point to overlapping flat_binder_object
    structs.
    
    Change-Id: I425ab0c46fbe2b00ed679c5becf9e8140395eb40
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    Git-commit: 457c3cd05958b8397211ae1f6dd3c3d325f4c0ea
    Git-repo: https://android.googlesource.com/kernel/common/
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -1400,7 +1400,7 @@ static void binder_transaction(struct binder_proc *proc,
 	struct binder_transaction *t;
 	struct binder_work *tcomplete;
 	binder_size_t *offp, *off_end;
-	size_t off_min;
+	binder_size_t off_min;
 	struct binder_proc *target_proc;
 	struct binder_thread *target_thread = NULL;
 	struct binder_node *target_node = NULL;
@@ -1606,10 +1606,10 @@ static void binder_transaction(struct binder_proc *proc,
 				*offp < off_min ||
 		    t->buffer->data_size < sizeof(*fp) ||
 		    !IS_ALIGNED(*offp, sizeof(u32))) {
- 			binder_user_error("%d:%d got transaction with invalid offset, %lld (min %zd, max %zd)\n",
+ 			binder_user_error("%d:%d got transaction with invalid offset, %lld (min %lld, max %lld)\n",
 					  proc->pid, thread->pid, (u64)*offp,
-					  off_min,
-					  (t->buffer->data_size -
+					  (u64)off_min,
+					  (u64)(t->buffer->data_size -
 					  sizeof(*fp)));
 			return_error = BR_FAILED_REPLY;
 			goto err_bad_offset;
