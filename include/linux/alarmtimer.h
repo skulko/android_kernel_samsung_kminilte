0d1ff909b852298fb0147e5befd399de20876aa5
Mohit Aggarwal <maggarwa@codeaurora.org>
2015-01-06 13:03:49 +0530

    rtc: alarm: Change wake-up source
    Currently, RTC_ALARM is used to wake-up target from
    suspend state and is also used for power-off alarm
    feature. This patch uses qtimer to wake-up from
    suspend state.
    
    Change-Id: Ia42cfecd573309be2f03c18b4f1c321be8202d7d
    Signed-off-by: Mohit Aggarwal <maggarwa@codeaurora.org>

--- include/linux/alarmtimer.h
+++ include/linux/alarmtimer.h
@@ -58,6 +58,9 @@ ktime_t alarm_expires_remaining(const struct alarm *alarm);
 
 /* Provide way to access the rtc device being used by alarmtimers */
 struct rtc_device *alarmtimer_get_rtcdev(void);
+#ifdef CONFIG_RTC_DRV_QPNP
+extern bool poweron_alarm;
+#endif
 
 /*
  * A alarmtimer is active, when it is enqueued into timerqueue or the
