899bfd97c2080810c5c69f98f074d27b238187cd
Johannes Berg <johannes.berg@intel.com>
2016-02-04 13:31:20 +0100

    ipv6: add option to drop unsolicited neighbor advertisements
    In certain 802.11 wireless deployments, there will be NA proxies
    that use knowledge of the network to correctly answer requests.
    To prevent unsolicitd advertisements on the shared medium from
    being a problem, on such deployments wireless needs to drop them.
    
    Enable this by providing an option called "drop_unsolicited_na".
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit aec215e7aa380fe5f85eb6948766b58bf78cb6c3)
    
    Change-Id: Iad429a767a786087b0985632be44932b2e3fd1a8
    
    # Conflicts:
    #	Documentation/networking/ip-sysctl.txt
    #	include/linux/ipv6.h
    #	net/ipv6/addrconf.c

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -4075,6 +4075,7 @@ static inline void ipv6_store_devconf(struct ipv6_devconf *cnf,
 	array[DEVCONF_FORCE_TLLAO] = cnf->force_tllao;
 	array[DEVCONF_ACCEPT_RA_MTU] = cnf->accept_ra_mtu;
 	array[DEVCONF_USE_OIF_ADDRS_ONLY] = cnf->use_oif_addrs_only;
+	array[DEVCONF_DROP_UNSOLICITED_NA] = cnf->drop_unsolicited_na;
 }
 
 static inline size_t inet6_ifla6_size(void)
@@ -4796,6 +4797,13 @@ static struct addrconf_sysctl_table
 			.mode           = 0644,
 			.proc_handler   = proc_dointvec,
 
+		},
+		{
+			.procname	= "drop_unsolicited_na",
+			.data		= &ipv6_devconf.drop_unsolicited_na,
+			.maxlen		= sizeof(int),
+			.mode		= 0644,
+			.proc_handler	= proc_dointvec,
 		},
 		{
 			/* sentinel */
