506a06949788c813d74bd5d721f880880eb519dc
Rick M <spookcity138@gmail.com>
2017-06-15 10:10:58 +0200

    defconfig update

--- kernel.sh
+++ kernel.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-
-export PATH="/usr/lib/ccache:$PATH"
-export CCACHE_DIR=/home/rick/.ccache
-
-
 export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/crosstool/arm-cortex_a7-UBERTC_8/bin/arm-eabi-
 
 make mrproper
 touch .scmversion
-make CC='ccache gcc' g800f_custom_defconfig
+make g800f_custom_defconfig
 make -j4
 
