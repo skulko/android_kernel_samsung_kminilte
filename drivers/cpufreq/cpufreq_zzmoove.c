3f55794ed0aaaf6596f71eff556582fea59a0dfd
Rick M <spookcity138@gmail.com>
2017-06-16 09:32:32 +0200

    fix some cpu governors after moving "get_cpu_idle_time()" to cpufreq.c

--- drivers/cpufreq/cpufreq_zzmoove.c
+++ drivers/cpufreq/cpufreq_zzmoove.c
@@ -2557,46 +2557,11 @@ static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
 }
 #endif /* LINUX_VERSION_CODE... */
 
-// ZZ: this function is placed here only from kernel version 3.4 to 3.8
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,4,0) && LINUX_VERSION_CODE < KERNEL_VERSION(3,8,0)
-static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
-{
-	u64 idle_time;
-	u64 cur_wall_time;
-	u64 busy_time;
-	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
-	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
-
-	idle_time = cur_wall_time - busy_time;
-	if (wall)
-	*wall = jiffies_to_usecs(cur_wall_time);
-	return jiffies_to_usecs(idle_time);
-}
-#endif /* LINUX_VERSION_CODE... */
-
 /*
  * ZZ: function has been moved out of governor since kernel version 3.8 and finally moved to cpufreq.c in kernel version 3.11
  *     overruling macro CPU_IDLE_TIME_IN_CPUFREQ included for sources with backported cpufreq implementation
  */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3,8,0) && !defined(CPU_IDLE_TIME_IN_CPUFREQ)
-static inline u64 get_cpu_idle_time(unsigned int cpu, u64 *wall)
-{
-	u64 idle_time = get_cpu_idle_time_us(cpu, NULL);
-
-	if (idle_time == -1ULL)
-		return get_cpu_idle_time_jiffy(cpu, wall);
-	else
-		idle_time += get_cpu_iowait_time_us(cpu, wall);
-
-	return idle_time;
-}
-#endif /* LINUX_VERSION_CODE... */
-
+ 
 // keep track of frequency transitions
 static int dbs_cpufreq_notifier(struct notifier_block *nb, unsigned long val, void *data)
 {
